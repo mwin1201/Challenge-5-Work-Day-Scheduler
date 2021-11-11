e7eac9a4bccaa6b57c78631e13f7453bd535c72d
Max W <mwinter@wesleyan.edu>
2021-11-11 13:26:20 -0600

    added local storage saving feature

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,17 @@
 // write current date to the Scheduler page
 $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
-var workHoursArr = [9,10,11,12,13,14,15,16,17];
+var dailyEvents = {
+    date: moment().format('dddd, MMMM Do YYYY'),
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: ""
+};
 
 var timeOfDay = function() {
     var currentHour = moment().hour();
@@ -17,9 +28,45 @@ var timeOfDay = function() {
                 textAreaEl[i].classList.add("present");
             }
         }
+        console.log("1 minute");
         // run the interval loop every minute
    // }, (1000 * 60));
 };
 
+var saveEvents = function(hour, text) {
+    var keepCurrentStorage = checkLocalStorage();
+    if (!keepCurrentStorage) {
+        dailyEvents[hour] = text;
+        localStorage.setItem("Scheduler", JSON.stringify(dailyEvents));
+    }
+    else {
+        var schedule = JSON.parse(localStorage.getItem("Scheduler"));
+        schedule[hour] = text;
+        localStorage.setItem("Scheduler", JSON.stringify(schedule));
+    }
+};
+
+var checkLocalStorage = function() {
+    var schedule = JSON.parse(localStorage.getItem("Scheduler"));
+    if (!schedule) {
+        return false;
+    }
+    else if (schedule.date !== dailyEvents.date) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+
+$(".saveBtn").click(function() {
+    var hour = $(this).parent().attr("id");
+    var text = $(this).parent().children("textarea").val();
+    if (!text) {
+        alert("You cannot save an empty event. Please enter something.");
+    }
+    saveEvents(hour, text);
+});
+
 timeOfDay();
-setInterval(timeOfDay, 1000);
+setInterval(timeOfDay, (1000 * 60));
